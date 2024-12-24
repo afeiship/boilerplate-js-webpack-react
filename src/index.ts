a2bfd111af9bdfb184532b7e9090b88b87d9f29d
afeiship <1290657123@qq.com>
2024-12-24 22:24:13 +0800

    feat: add typings

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import CryptoJS from 'crypto-js';
 
-async function file2wordArray(file) {
+async function file2wordArray(file: File): Promise<CryptoJS.lib.WordArray> {
   const arrayBuffer = await file.arrayBuffer();
   return CryptoJS.lib.WordArray.create(arrayBuffer);
 }
 
-export async function computeMD5(file) {
+export async function computeMD5(file: File) {
   const wordArray = await file2wordArray(file);
   return CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
 }
 
-export async function computeSHA256(file) {
+export async function computeSHA256(file: File) {
   const wordArray = await file2wordArray(file);
   return CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
 }
