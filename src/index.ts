8560177a9bce7dab941b22153c508a3fcbb6d181
afeiship <1290657123@qq.com>
2024-12-24 23:07:06 +0800

    feat: file->blob for input

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import CryptoJS from 'crypto-js';
 
-async function file2wordArray(file: File): Promise<CryptoJS.lib.WordArray> {
+async function file2wordArray(file: Blob): Promise<CryptoJS.lib.WordArray> {
   const arrayBuffer = await file.arrayBuffer();
   return CryptoJS.lib.WordArray.create(arrayBuffer);
 }
 
-export async function computeMD5(file: File) {
+export async function computeMD5(file: Blob) {
   const wordArray = await file2wordArray(file);
   return CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
 }
 
-export async function computeSHA256(file: File) {
+export async function computeSHA256(file: Blob) {
   const wordArray = await file2wordArray(file);
   return CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
 }
